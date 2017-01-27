496f40f0212953273053e38078d36cba30f0ab31
juliusbuckley <juliusbuckley@gmail.com>
2017-01-27 09:00:25 -0800

    Create GET /events

---
+++ server/routes/amplitudeRoutes.js
@@ -0,0 +1,4 @@
+import amplitudeController from '../controllers/amplitudeController';
+// API ROUTES
+const amplitudeRoutes = (app) => { app.get('/events', amplitudeRoutes); };
+export default amplitudeRoutes;
