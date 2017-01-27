e9aa084830f046406e1733f6324ee0ea998ce8c3
juliusbuckley <juliusbuckley@gmail.com>
2017-01-27 14:45:39 -0800

    Create test to confirm events are sending to amplitude

--- server/test/routeSpec.js
+++ server/test/routeSpec.js
@@ -2,10 +2,10 @@ import app from '../app.js';
 import request from 'supertest';
 import { expect } from 'chai';
 
-describe('events test', () => {
-  it('expect GET /events to return 200', (done) => {
+describe('GET /test', () => {
+  it('should return 200 to confirm events are sent to amplitude', done => {
     request(app)
-      .get('/events')
+      .get('/test')
       .expect(200, done);
   });
 });
