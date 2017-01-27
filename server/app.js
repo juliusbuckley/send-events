2a22fc5d0f19a5c4caee427854cd442575e537fa
juliusbuckley <juliusbuckley@gmail.com>
2017-01-27 13:40:07 -0800

    Serve static routes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import path from 'path';
 import amplitudeRoutes from './routes/amplitudeRoutes';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.disable('x-powered-by');
-
+app.use(express.static(path.join(__dirname, '../client')));
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css'))); 
 amplitudeRoutes(app);
 
 app.listen(port, () => { console.log(`app.js has been served on port: ${port}`); });
