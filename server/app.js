d78fa4594829cc067ae0102664f1d7bf17673eab
juliusbuckley <juliusbuckley@gmail.com>
2017-01-27 16:03:49 -0800

    Add test route

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import path from 'path';
-import amplitudeRoutes from './routes/amplitudeRoutes';
+import { amplitudeRoutes, testRoutes } from './routes/amplitudeRoutes';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.disable('x-powered-by');
 app.use(express.static(path.join(__dirname, '../client')));
-app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css'))); 
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
+
+// ROUTES
 amplitudeRoutes(app);
+testRoutes(app);
 
 app.listen(port, () => { console.log(`app.js has been served on port: ${port}`); });
+
+export default app;
