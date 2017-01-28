cdfd28a6c8c84103f5c4380813280ecf1447f066
juliusbuckley <juliusbuckley@gmail.com>
2017-01-27 16:06:16 -0800

    Add test route to controller

--- server/routes/amplitudeRoutes.js
+++ server/routes/amplitudeRoutes.js
@@ -1,4 +1,6 @@
-import amplitudeController from '../controllers/amplitudeController';
+import { readEvents } from '../controllers/amplitudeController';
+import testController from '../controllers/testController';
 // API ROUTES
-const amplitudeRoutes = (app) => { app.get('/events', amplitudeController); };
-export default amplitudeRoutes;
+const amplitudeRoutes = (app) => { app.get('/events', readEvents); };
+const testRoutes = (app) => { app.get('/test', testController); };
+export { amplitudeRoutes, testRoutes };
