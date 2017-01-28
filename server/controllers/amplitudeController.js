7dffb59788ad81bc95e3b328956280af49e953ca
juliusbuckley <juliusbuckley@gmail.com>
2017-01-27 16:06:43 -0800

    Create readEvent function

--- server/controllers/amplitudeController.js
+++ server/controllers/amplitudeController.js
@@ -1,17 +1,31 @@
 import axios from 'axios';
 import qs from 'querystring';
+import fs from 'fs';
+import readline from 'readline';
 // SEND POST REQUEST TO AMPLITUDE API
-const eventData = {"user_id": "able_doe@gmail.com", "event_type": "watch_test", "user_properties": {"Cohort": "Test A"}, "country": "United States", "ip": "127.0.1.1", "time": 1396381378126};
-const postEvents = (req, res) => {
+const postEvent = (eventData) => {
+  // CHANGE KEY VALUES TO BE COMPATIBLE WITH AMPLITUDE
+  eventData = eventData.replace('event', 'event_type');
+  eventData = eventData.replace('userId', 'user_id');
   const apiUrl = 'https://api.amplitude.com/httpapi?';
   const data = {
     api_key: process.env.AMPLITUDE_API_KEY,
-    event: JSON.stringify([eventData])
+    event: eventData
   };
   axios.post(apiUrl, qs.stringify(data))
     .then((data) => {
-      res.send('event sent!');
+      console.log('event sent!');
     })
     .catch((err) => { console.error(`Error: ${err.response.data}`); });
 };
-export default postEvents;
+// READ events.txt
+const readEvents = (req, res) => {
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream('events.txt')
+  });
+  lineReader.on('line', line => {
+    postEvent(line);
+  });
+  res.send('Events posting...');
+};
+export { readEvents, postEvent };
